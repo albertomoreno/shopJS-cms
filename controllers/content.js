641a12d7c7e8c02d9e05267cbb758f64c7acbdf7
alberto <albertinivva@gmail.com>
2016-03-13 15:56:28 +0100

    Faqs page.

--- controllers/content.js
+++ controllers/content.js
@@ -5,8 +5,8 @@ var express = require('express'),
     template = require('../lib/template.js');
 
 module.exports = {
-  home: function(req, res) {
-    template.render(res, 'home/home');
+  faq: function(req, res) {
+    template.render(res, 'content/faq');
   },
 };
 
--- router.js
+++ router.js
@@ -1,7 +1,11 @@
 
 var home = require('./controllers/home');
+var content = require('./controllers/content');
 
 
 module.exports = function(app) {
+  app.use('/faq', content.faq);
   app.use('/', home.home);
 };
+
+
