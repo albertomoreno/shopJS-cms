d34e8f7e58e72317bdd3a18d57dd26dd9bc5bd0e
alberto <albertinivva@gmail.com>
2016-05-27 18:00:31 +0200

    Separate admin navbar.

---
+++ models/Category.js
@@ -0,0 +1,21 @@
+
+'use strict';
+
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var schema = new Schema({
+  name: { type: String, required: true, unique: true  },
+  parent: { type: Number, required: true},
+  published: { type: Boolean, required: true },
+  slug: { type: String, required: true },
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Category = mongoose.model('Category', schema);
+
+// make this available to our users in our Node applications
+module.exports = Category;
